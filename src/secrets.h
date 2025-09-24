535044a4735a9a2dd3cc9137c43eccd10db031b9
Jory81 <joryp@planet.nl>
2025-09-24 10:23:17 +0200

    Update secrets.h

---
+++ src/secrets.h
@@ -0,0 +1,10 @@
+// src/secrets.h   <-- DO NOT COMMIT
+#pragma once
+
+// Your real credentials (keep these local)
+//#define WIFI_SSID     "Supersnel"
+//#define WIFI_PASSWORD "sesamstraat13"
+
+const char* MQTT_HOST   = "192.168.2.154";
+const char* MQTT_USER   = "princiej";
+const char* MQTT_PASS   = "Az81!PrinsJ";
