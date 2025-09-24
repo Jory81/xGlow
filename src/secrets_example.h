45996addcfec6cabd3e777493d344b6d79dd42f4
Jory81 <joryp@planet.nl>
2025-09-24 10:57:02 +0200

    update
    update

---
+++ src/secrets_example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// #define WIFI_SSID     "YourHomeSSID"
+// #define WIFI_PASSWORD "YourSuperSecretPass"
+
+const char* MQTT_HOST   = "hostname";
+const char* MQTT_USER   = "username";
+const char* MQTT_PASS   = "password";
