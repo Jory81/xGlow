1b9ca69405e685de2b2d3d2e74443619d5bac65a
Jory81 <joryp@planet.nl>
2025-09-24 10:31:15 +0200

    Add secrets.h template

---
+++ src/secrets.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//#define WIFI_SSID     "YOUR_WIFI_SSID"
+//#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+const char* MQTT_HOST   = "192.168.2.154";
+const char* MQTT_USER   = "princiej";
+const char* MQTT_PASS   = "Az81!PrinsJ";
